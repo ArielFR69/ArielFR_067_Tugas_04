27b8bff32bef68d142af5f5c5fd91a553b049a90
Ariel FR <90166724+ArielFR69@users.noreply.github.com>
2021-10-22 03:05:59 +0700

    Add files via upload

---
+++ ArielFikriRamadhani_067_Penjualan goto.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+
+int main()
+
+{
+    
+    //Data Declaration
+
+    start :
+	int i, n, total, price = 0, pre;
+	float disc;
+    char obj;
+
+   
+	cout<< "--------------------------------------------------" << endl;
+	cout<< "       Program Penghitungan Penjualan Total       " << endl;
+	cout<< "--------------------------------------------------" << endl;
+	
+
+    //Data Input
+
+	cout<< "Total Jumlah Jenis Barang : "; cin >> n;
+	cout<< endl;
+
+	for(i=1; i<=n; i++){
+		cout<< "Jumlah Harga Barang Ke-" << i << ": ";cin >> pre;
+		price = price+pre;
+	}
+		if(price > 500){
+			disc = price - (price*10/100);			
+		}
+		else if(price >= 200 && price <= 500){
+			disc = price - (price*5/100);
+		}
+		else {
+			disc = price;
+		}
+		
+        cout << endl;
+
+
+    //Result
+
+	
+	cout << "--------------------------------------------------" << endl;	
+	cout << "Total Harga Asli = Rp" << price << endl;
+	cout << "Total Harga Setelah Diskon = Rp" << disc << endl;
+	cout << "--------------------------------------------------" << endl;	
+    cout << endl;
+
+
+    cout << "Apakah anda ingin menghitung total penjualan barang lagi? [Y/N] ="; cin>>obj;
+    if (obj== 'Y' || obj== 'y'){
+        goto start;
+    }
+    else if (obj== 'N' || obj== 'n'){
+        exit( 0 );
+    }
+}
